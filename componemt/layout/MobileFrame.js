8d9532c99f2c3bb940860051b1a28a73f75386d0
EmmaYeh023 <152198164+EmmaYeh023@users.noreply.github.com>
2025-05-10 20:38:58 +0800

    update0508

--- componemt/layout/MobileFrame.js
+++ componemt/layout/MobileFrame.js
@@ -6,7 +6,7 @@ export default function MobileFrame({children}) {
 
   return (
    <>
-    <div className="w-[393px] h-[852px]  bg-white rounded-2x1 flex justify-center items-center ">
+    <div className=" w-[33%] min-w-[380px]  h-[95%] min-h-[750px] p-[52px] bg-white flex justify-center items-center rounded-2xl relative overflow-hidden   ">
         {children}
       </div>
    </>
--- componemt/page/QuizPage.js
+++ componemt/page/QuizPage.js
@@ -1,15 +1,29 @@
 'use client';
-import MobileFrame from '@/componemt/layout/MobileFrame'
+import MobileFrame from '@/componemt/layout/MobileFrame';
+import Image from 'next/image';
+import titleImg from '@/public/0.start/title.svg';
+import startBtn from '@/public/0.start/startbtn.png';
+import circleY from '@/public/0.start/blur-circleY.png';
 
-export default function QuizPage(quizIndex) {
-  
-
+export default function QuizPage({questionIndex, nextStep }) {
   return (
-   <>
-       <MobileFrame>
-        QuizPage:Q{quizIndex}
-      </MobileFrame>
-   </>
- 
+    <MobileFrame>
+      QuizPage: Q{questionIndex+1}x
+
+      <div className= 'flex justify-center items-center flex-col gap-[60px]'>
+        <Image className=' w-[450px] absolute top-0 -translate-y-1/2 ' src={circleY} alt='circleY' />
+        <Image src={titleImg} alt='title' />
+        <div className= 'text-[#B95F0F] font-[500] text-center text-[14px] leading-loose '>
+            有些人天生酥脆，有些人出爐時就塌了。
+            你努力發酵、翻滾、等待出爐，
+            結果還是變成一坨可頌災難。
+            沒關係，這世界不缺完美麵包，
+            缺的是——像你一樣軟爛卻獨特的存在。
+            現在，就來看看你是什麼等級的失控可頌吧。
+        </div>
+        <Image onClick={nextStep} className= 'w-[160px]' src={startBtn} alt='starBtn' />
+        <Image className=' absolute bottom-0 translate-y-1/2 w-[450px]' src={circleY} alt='circleY' />
+      </div>
+    </MobileFrame>
   );
 }
